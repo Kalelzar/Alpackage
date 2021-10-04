6901bb206d657ff5bdf97f9655eb78d37b283025
Kalelzar <natomanofglory@gmail.com>
2021-10-04 15:08:39 +0300

    More build files

---
+++ src/ModuleLoader.cpp
@@ -0,0 +1,59 @@
+#include <Alpackage/Module/Module.hpp>
+#include <Alpackage/Module/ModuleLoader.hpp>
+#include <Alpackage/Util/Logging.hpp>
+#include <boost/dll/import.hpp>
+
+namespace Alpackage::Module {
+
+using ModulePtr = boost::shared_ptr<IAlpackageModule>;
+
+ModulePtr ModuleLoader::load (std::string const& moduleName) {
+  return load (moduleName, "/usr/lib/alpackage/");
+}
+
+ModulePtr ModuleLoader::load (std::string const&          moduleName,
+                              boost::dll::fs::path const& prefix) {
+  Log::info ("Loading %s from '%s'", moduleName, prefix.string ( ));
+
+
+  boost::shared_ptr<Alpackage::Module::IAlpackageModule> module;
+
+  module = boost::dll::import<Alpackage::Module::IAlpackageModule> (
+
+    prefix / moduleName,
+    "module",
+    boost::dll::load_mode::append_decorations
+
+  );
+
+  auto result = module->init ( );
+
+  if (result != ModuleError::NONE) {
+    Log::error ("Failed to initialize module: %s", moduleName);
+    throw std::runtime_error ("Failed to initialize module.");
+  }
+
+  Log::info ("Successfully initialized: %s", moduleName);
+
+  Log::debug ("Module %s v%s", module->name ( ), module->version ( ));
+  {
+    reckless::scoped_indent indent;
+    Log::debug ("Install: %s", module->canInstall ( ) ? "yes" : "no");
+    Log::debug ("Search: %s", module->canSearch ( ) ? "yes" : "no");
+    Log::debug ("Find: %s", module->canFind ( ) ? "yes" : "no");
+    Log::debug ("List: %s", module->canList ( ) ? "yes" : "no");
+
+    return module;
+  }
+}
+
+ModulePtr ModuleLoader::getModule (std::string moduleName) {
+  return load (moduleName);
+}
+
+ModulePtr ModuleLoader::getModule (std::string                 moduleName,
+                                   boost::dll::fs::path const& prefix) {
+  return load (moduleName, prefix);
+}
+
+}     // namespace Alpackage::Module
---
+++ test/EntryReader.cpp
@@ -0,0 +1,45 @@
+#include <Alpackage/Util/EntryReader.hpp>
+#include <gtest/gtest.h>
+#include <rapidcheck/Assertions.h>
+#include <rapidcheck/gtest.h>
+#include <sstream>
+
+
+RC_GTEST_PROP (EntryReader, TestReadInt, (int a, int b, int c)) {
+  std::stringstream s;
+  s << a << " " << b << " " << c;
+  auto v = EntryReader<int>::parse (&s);
+  RC_ASSERT (v.size ( ) == 3);
+  RC_ASSERT (v[0] == a);
+  RC_ASSERT (v[1] == b);
+  RC_ASSERT (v[2] == c);
+  RC_ASSERT (s.eof ( ));
+}
+
+class ThreeNumbers {
+  public:
+  int a, b, c;
+  ThreeNumbers ( ) : a (0), b (0), c (0) { }
+
+  friend std::istream& operator>> (std::istream& in, ThreeNumbers& nums) {
+    in >> nums.a >> nums.b >> nums.c;
+    return in;
+  }
+};
+
+RC_GTEST_PROP (EntryReader, TestReadStruct, (int a, int b, int c)) {
+  std::stringstream s;
+  s << a << " " << b << " " << c;
+  auto v = EntryReader<ThreeNumbers>::parse (&s);
+  RC_ASSERT (v.size ( ) == 1);
+  RC_ASSERT (v[0].a == a);
+  RC_ASSERT (v[0].b == b);
+  RC_ASSERT (v[0].c == c);
+  RC_ASSERT (s.eof ( ));
+}
+
+RC_GTEST_PROP (EntryReader, TestReadStructFail, (int a, int b)) {
+  std::stringstream s;
+  s << a << " " << b;
+  RC_ASSERT_THROWS (EntryReader<ThreeNumbers>::parse (&s));
+}
