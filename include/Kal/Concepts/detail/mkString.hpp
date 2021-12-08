74b8a86d12424641959f718c8718e628cefe4223
Kalelzar <natomanofglory@gmail.com>
2021-12-08 15:11:09 +0200

    Expand mkString signature

--- include/Kal/Concepts/detail/mkString.hpp
+++ include/Kal/Concepts/detail/mkString.hpp
@@ -4,42 +4,55 @@
 
 #include <sstream>
 
-template<Iterable T>
+template<Iterable    T,
+         OStreamable Delim,
+         OStreamable Before = std::string,
+         OStreamable After  = std::string>
   requires (!ConstIterable<T>)
 &&OStreamable<typename IteratesWith<T>::type> std::string
-  mkString (T const& t, std::string delim) {
+                                              mkString (T const&      t,
+                                                        Delim const&  delim,
+                                                        Before const& before = "[ ",
+                                                        After const&  after = " ]") {
   std::stringstream s;
-  s << "[ ";
+  s << before;
   for (auto it = t.begin ( ); it != t.end ( ); ++it) {
     s << *it;
     if (std::next (it) != t.end ( )) s << delim;
   }
-  s << " ]";
+  s << after;
   return std::move (s.str ( ));
 }
 
-
-template<ConstIterable T>
+template<ConstIterable T,
+         OStreamable   Delim,
+         OStreamable   Before = std::string,
+         OStreamable   After  = std::string>
   requires OStreamable<typename IteratesWith<T>::type>
-inline std::string mkString (T const& t, std::string delim) {
+inline std::string mkString (T const&      t,
+                             Delim const&  delim,
+                             Before const& before = "[ ",
+                             After const&  after  = " ]") {
   std::stringstream s;
-  s << "[ ";
+  s << before;
   auto it = t.cbegin ( );
   if (it != t.cend ( )) {
     s << *it;
     ++it;
   }
   for (; it != t.cend ( ); ++it) { s << delim << *it; }
-  s << " ]";
+  s << after;
   return std::move (s.str ( ));
 }
 
 template<AnyIterable T>
-  requires OStreamable<typename IteratesWith<T>::type>
-inline std::string mkString (T const& t) { return mkString (t, " "); }
+  requires OStreamable<typename IteratesWith<T>::type> &&(
+    !OStreamable<T>) inline std::string mkString (T const& t) {
+    return mkString (t, " ");
+  }
 
-template<OStreamable C> std::string mkString (C const& t) {
-  std::stringstream s;
-  s << t;
-  return s.str ( );
-}
+  template<OStreamable C> std::string mkString (C const& t) {
+    std::stringstream s;
+    s << t;
+    return s.str ( );
+  }
