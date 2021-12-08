9fffddb1b621bee6aa6ac6915ddcad2995dcd92d
Kalelzar <natomanofglory@gmail.com>
2021-12-08 15:07:01 +0200

    Remove superfluous compiler flags

--- cmake/CodeCoverage.cmake
+++ cmake/CodeCoverage.cmake
@@ -162,10 +162,10 @@ set(COVERAGE_COMPILER_FLAGS "-g -fprofile-arcs -ftest-coverage"
     CACHE INTERNAL "")
 if(CMAKE_CXX_COMPILER_ID MATCHES "(GNU|Clang)")
     include(CheckCXXCompilerFlag)
-    check_cxx_compiler_flag(-fprofile-abs-path HAVE_fprofile_abs_path)
-    if(HAVE_fprofile_abs_path)
-        set(COVERAGE_COMPILER_FLAGS "${COVERAGE_COMPILER_FLAGS} -fprofile-abs-path")
-    endif()
+#    check_cxx_compiler_flag(-fprofile-abs-path HAVE_fprofile_abs_path)
+#    if(HAVE_fprofile_abs_path)
+#        set(COVERAGE_COMPILER_FLAGS "${COVERAGE_COMPILER_FLAGS} -fprofile-abs-path")
+#    endif()
 endif()
 
 set(CMAKE_Fortran_FLAGS_COVERAGE
