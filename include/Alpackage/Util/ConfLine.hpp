de51b4aca83a8ffcf1779afc9f8ccd89032d2ca6
Kalelzar <natomanofglory@gmail.com>
2021-12-13 19:03:21 +0200

    Convert ConfLine to read-style Entry
    Now with 100% less exceptions

--- include/Alpackage/Util/ConfLine.hpp
+++ include/Alpackage/Util/ConfLine.hpp
@@ -2,11 +2,15 @@
 
 #include <Alpackage/Util/Logging.hpp>
 
+#include <Kal/ErrorOr.hpp>
+#include <Kal/Format.hpp>
+
 #include <cstring>
 #include <istream>
 #include <sstream>
 #include <stdexcept>
 #include <string>
+
 
 struct ConfLine {
   std::string name;
@@ -81,6 +85,71 @@ struct ConfLine {
     Log::debug ("Pass rest of buffer to value: %s", buffer + point);
     Log::flush ( );
     return in;
+  }
+
+  static ErrorOr<ConfLine> read (std::istream* in) {
+    Log::debug ("Parsing ConfLine.");
+    reckless::scoped_indent indent;
+    static const size_t     bufferSize = 512;
+    char                    buffer[bufferSize];
+    std::stringstream       name;
+    std::stringstream       value;
+    int                     point = 0;
+    int                     mark  = 0;
+    size_t                  len   = 0;
+
+    do {
+      in->getline (buffer, bufferSize);
+      len = strlen (buffer);
+    } while (len == 0);
+
+    Log::debug ("Acquired line of length %d: %s", len, buffer);
+    while (point < len && isWhitespace (buffer[point])) { point++; }
+    Log::debug ("Discarded '%d' white space chars: %s", point, buffer + point);
+    mark = point;
+
+    while (point < len && !isWhitespace (buffer[point])
+           && buffer[point] != '=') {
+      point++;
+    }
+
+    Log::debug ("Skipped '%d' non-whitespace chars: %s",
+                point - mark,
+                buffer + point);
+
+    char cbuf[bufferSize];
+    std::strncpy (cbuf, buffer + mark, point - mark);
+    cbuf[point - mark] = '\0';
+    Log::debug ("Copy skipped chars in separate buffer: %s", cbuf);
+
+    name << cbuf;
+
+    mark = point;
+    while (point < len && isWhitespace (buffer[point])) { point++; }
+    Log::debug ("Discarded '%d' white space chars: %s",
+                point - mark,
+                buffer + point);
+
+    if (point < len && buffer[point] != '=') {
+      return format ("Invalid ConfLine; While parsing name, found: '{}' where "
+                     "'=' was expected.",
+                     buffer[point]);
+    }
+
+    point++;
+
+    Log::debug ("Skipped '=': %s", cbuf);
+    mark = point;
+    while (point < len && isWhitespace (buffer[point])) { point++; }
+    Log::debug ("Discarded '%d' white space chars: %s",
+                point - mark,
+                buffer + point);
+    value << (buffer + point);
+
+
+    Log::debug ("Pass rest of buffer to value: %s", buffer + point);
+    Log::flush ( );
+    return ConfLine{name.str ( ), value.str ( )};
   }
 
   private:
