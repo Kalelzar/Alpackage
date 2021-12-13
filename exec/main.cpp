b15b77c5acbd8a4df72406b8d98cb3bf07a400f3
Kalelzar <natomanofglory@gmail.com>
2021-12-13 20:21:19 +0200

    Have main make use of the new features

--- exec/main.cpp
+++ exec/main.cpp
@@ -7,22 +7,37 @@
 #include <Kal/Concepts/Applicators.hpp>
 
 #include <Kal/Flag.hpp>
+#include <Kal/View/detail/mkString.hpp>
 #include <Kal/XDGBaseDir.hpp>
 
 #include <boost/dll/import.hpp>
 #include <iostream>
+#include <ranges>
 #include <stdexcept>
 
 #define KAL_UNHANDLED_ERROR 255
 
-ErrorOr<int> kalmain (int argc, char** argv) { return 0; }
+ErrorOr<int> kalmain (int argc, char** argv) {
+  auto textdb
+    = TRY (Alpackage::Module::ModuleLoader::getModule ("AlpackageTextDB"));
+  auto git = TRY (Alpackage::Module::ModuleLoader::getModule ("AlpackageGit"));
 
-int          main (int argc, char** argv) {
+  std::cout << "Checking git packages status:" << std::endl
+            << mkString (TRY (git->hasUpdates ( )), '\n');
+
+  return 0;
+}
+
+int main (int argc, char** argv) {
   auto t = kalmain (argc, argv);
+
+
   if (t.isEmpty ( )) {
-    std::cerr << t << std::endl;
+    Log::error ("%s", mkString (t));
     return KAL_UNHANDLED_ERROR;
   }
 
   return t.get ( );
 }
+
+#undef KAL_UNHANDLED_ERROR
